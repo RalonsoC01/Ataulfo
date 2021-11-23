868a71a684b0b646a78ee4c351f410b58005895b
RalonsoC01 <72815754+RalonsoC01@users.noreply.github.com>
2021-11-23 20:57:15 +0100

    Map

--- app/src/main/java/com/example/ataulfo/informacion.java
+++ app/src/main/java/com/example/ataulfo/informacion.java
@@ -2,7 +2,9 @@ package com.example.ataulfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class informacion extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class informacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
+    }
+    public void map (View view)
+    {
+        Intent i= new Intent(this,MapsActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
     }
 }
