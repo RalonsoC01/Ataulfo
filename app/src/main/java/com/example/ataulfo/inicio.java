74f39b198094e058b9eb910c0ea719c80988ff4a
DAM202 <ralonsoc01@educantabria.es>
2021-11-23 16:40:36 +0100

    23-11-2021

--- app/src/main/java/com/example/ataulfo/inicio.java
+++ app/src/main/java/com/example/ataulfo/inicio.java
@@ -2,7 +2,9 @@ package com.example.ataulfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class inicio extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+    public void Informacion(View view)
+    {
+        Intent i= new Intent(this,informacion.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
+    }
+    public void eso (View view)
+    {
+        Intent i= new Intent(this,eso.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
+    }
+    public void bachiller(View view)
+    {
+        Intent i= new Intent(this,bachiller.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
+    }
+    public void fp (View view)
+    {
+        Intent i= new Intent(this,FP.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
     }
 }
